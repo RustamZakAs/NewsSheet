6103b7072e8c09d08a0cea294200b8b333d3d09e
Rustam Zak <rostik055@gmail.com>
2019-01-08 16:39:00 +0400

    up

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -11,17 +14,73 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
+using System.Drawing;
 
 namespace NewsSheet
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void Set<T>(ref T field, T value, [System.Runtime.CompilerServices.CallerMemberName]string prop = "")
+        {
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private ObservableCollection<News> newsList = new ObservableCollection<News>();
+        public ObservableCollection<News> NewsList { get => newsList; set => Set(ref newsList, value); }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            //XmlDeSerialize();
+
+            News news = new News()
+            {
+                Id = 1,
+                NImageLink = @"\mainicon.ico",
+                NPriority = 99,
+                NLabel = "One News",
+                NDateTime = DateTime.Now,
+                NContent = "Текст новости!!!"
+            };
         }
+
+        void XmlSerialize()
+        {
+            // передаем в конструктор тип класса
+            XmlSerializer formatter = new XmlSerializer(typeof(ObservableCollection<News>));
+
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream("News.xml", FileMode.Create))
+            {
+                formatter.Serialize(fs, NewsList);
+            }
+        }
+
+        void XmlDeSerialize()
+        {
+            // передаем в конструктор тип класса
+            XmlSerializer formatter = new XmlSerializer(typeof(ObservableCollection<News>));
+            // десериализация
+            using (FileStream fs = new FileStream("News.xml", FileMode.OpenOrCreate))
+            {
+                try
+                {
+                    NewsList = (ObservableCollection<News>)formatter.Deserialize(fs);
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    MessageBox.Show(ioe.Message);
+                }
+            }
+        }
+
     }
 }
---
+++ News.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace NewsSheet
+{
+    [Serializable]
+    public class News : INotifyPropertyChanged
+    {
+        public News()
+        { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void Set<T>(ref T field, T value, [System.Runtime.CompilerServices.CallerMemberName]string prop = "")
+        {
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private int id;
+        public int Id { get => id; set => Set(ref id, value); }
+
+        private string nImageLink;
+        public string NImageLink { get => nImageLink; set => Set(ref nImageLink, value); }
+
+        private int nPriority;
+        public int NPriority { get => nPriority; set => Set(ref nPriority, value); }
+
+        private string nLabel;
+        public string NLabel { get => nLabel; set => Set(ref nLabel, value); }
+
+        private DateTime nDateTime;
+        public DateTime NDateTime { get => nDateTime; set => Set(ref nDateTime, value); }
+
+        private string nContent;
+        public string NContent { get => nContent; set => Set(ref nContent, value); }
+    }
+}
